e80b7b5bc72684e91936034fb5c23b38a194871b
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-03 15:41:10 -0500

    [Feature] Flesh out operation tests

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -1,4 +1,8 @@
 const sum = require("../calculator.js")
 
-
-test("Sums two integers", () =>{expect(sum(2,3)).toBe(5);});
+describe("Operations", () =>{
+  test("Sums two integers", () =>{expect(sumNumbers(2,3)).toBe(5);});
+  test.skip("Takes away one int from the other", () =>{expect(takeAwayNumbers(5,3)).toBe(2);});
+  test.skip("Multiplies two integers", () =>{expect(multiplyNumbers(2,3).toBe(6))});
+  test.skip("Divides two integers", () => {expect(dividesNumbers(6,3).toBe(2))});
+});
