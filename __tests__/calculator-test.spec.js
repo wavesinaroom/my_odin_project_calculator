c688d67bf25a5f5806c3d0cba9e1ae39a28614d6
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-03 18:15:20 -0500

    [Fix] Change reference in tests to actually call the operator function

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -17,9 +17,9 @@ describe("Divide", () =>{
 });
 
 describe("Operate", () =>{
-  test.skip("Sums two integers", () =>{expect(calculator.operate(2,3,"+")).toBe(5);});
+  test("Sums two integers", () =>{expect(calculator.operate(2,3,"+")).toBe(5);});
   test.skip("Takes away one int from the other", () =>{expect(calculator.operate(5,3,"-")).toBe(2);});
-  test.skip("Multiplies two integers", () =>{expect(calculator.multiplyNumbers(2,3,"*")).toBe(6);});
-  test.skip("Divides two integers", () => {expect(calculator.dividesNumbers(6,3,"/")).toBe(2);});
+  test.skip("Multiplies two integers", () =>{expect(calculator.operate(2,3,"*")).toBe(6);});
+  test.skip("Divides two integers", () => {expect(calculator.operate(6,3,"/")).toBe(2);});
   test.skip("Throws error if operant isn't right", () =>{expect(calculator.operate(6,3,"#").toBe("Wrong operator input"))});
 });
