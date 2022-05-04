f9873972659a811cea83f495f5af6b32b3a2fe0f
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-04 10:46:52 -0500

    [Feature] Pass operate take away test

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -18,7 +18,7 @@ describe("Divide", () =>{
 
 describe("Operate", () =>{
   test("Sums two integers", () =>{expect(calculator.operate(2,3,"+")).toBe(5);});
-  test.skip("Takes away one int from the other", () =>{expect(calculator.operate(5,3,"-")).toBe(2);});
+  test("Takes away one int from the other", () =>{expect(calculator.operate(5,3,"-")).toBe(2);});
   test.skip("Multiplies two integers", () =>{expect(calculator.operate(2,3,"*")).toBe(6);});
   test.skip("Divides two integers", () => {expect(calculator.operate(6,3,"/")).toBe(2);});
   test.skip("Throws error if operant isn't right", () =>{expect(calculator.operate(6,3,"#").toBe("Wrong operator input"))});
