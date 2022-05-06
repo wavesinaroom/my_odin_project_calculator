142a3ce3aea38f223588c30d947edb8e7697bc3c
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-06 15:20:30 -0500

    [Task] Refactor tests to individual suites

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -1,15 +1,22 @@
 const calculator = require("../calculator.js");
 
-describe("Calculate two numbers", () =>{
-  beforeEach(()=>{
-    calculator.numbers = [];
-    calculator.symbols = [];
-  })
-
+describe("Addition", () =>{
   test("Sums two integers", () =>{expect(calculator.compute("2+3")).toBe(5);});
-  test("Sums two integers", () =>{expect(calculator.compute("2+2")).toBe(4);});
-  test("Takes away one int from the other", () =>{expect(calculator.compute("5-3")).toBe(2);});
-  test("Multiplies two integers", () =>{expect(calculator.compute("2*3")).toBe(6);});
-  test("Divides two integers", () => {expect(calculator.compute("6/3")).toBe(2);});
-  test("Throws error if operant isn't right", () =>{expect(calculator.compute("#")).toBe("Wrong operator input");});
+  test("Sums more than two integers", () =>{expect(calculator.compute("2+3")).toBe(5);});
+});
+
+describe("Substraction", () =>{
+  test.skip("Takes away one int from the other", () =>{expect(calculator.compute("5-3")).toBe(2);});
+});
+
+describe("Multiplication", () =>{
+  test.skip("Multiplies two integers", () =>{expect(calculator.compute("2*3")).toBe(6);});
+});
+
+describe("Division", () =>{
+  test.skip("Divides two integers", () => {expect(calculator.compute("6/3")).toBe(2);});
+});
+
+describe("Wrong input", () =>{
+  test.skip("Throws error if operant isn't right", () =>{expect(calculator.compute("#")).toBe("Wrong operator input");});
 });
