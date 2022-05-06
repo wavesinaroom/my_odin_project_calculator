b9c1ca13fd04eca3e2c3d26acff988eaac200289
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-06 17:00:05 -0500

    [Feature] Write tests for the rest of operations - input test pending

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -9,15 +9,27 @@ describe("Addition", () =>{
 });
 
 describe("Substraction", () =>{
-  test.skip("Takes away one int from the other", () =>{expect(calculator.compute("5-3")).toBe(2);});
+  test.skip("Takes away two integers", () =>{expect(calculator.compute("5-3")).toBe(2);});
+  test.skip("Takes away even number of integers", () =>{expect(calculator.compute("10-3-4-2")).toBe(1);});
+  test.skip("Takes away odd number of integers", () =>{expect(calculator.compute("5-3-2")).toBe(0);});
+  test.skip("Takes away large integers", () =>{expect(calculator.compute("321564-3541-354-1000")).toBe(316669);});
+  test.skip("Takes away floats", () =>{expect(calculator.compute("45.285-20.15-5.54")).toBe(19.595);});
 });
 
 describe("Multiplication", () =>{
   test.skip("Multiplies two integers", () =>{expect(calculator.compute("2*3")).toBe(6);});
+  test.skip("Multiplies even number of integers", () =>{expect(calculator.compute("2*3*4*8")).toBe(192);});
+  test.skip("Multiplies odd number of integers", () =>{expect(calculator.compute("2*3*5")).toBe(30);});
+  test.skip("Multiplies large numbers", () =>{expect(calculator.compute("204*36*456*65")).toBe(217676160);});
+  test.skip("Multiplies floats", () =>{expect(calculator.compute("2.32*3.78*5.65")).toBe(49.54824);});
 });
 
 describe("Division", () =>{
   test.skip("Divides two integers", () => {expect(calculator.compute("6/3")).toBe(2);});
+  test.skip("Divides even number of integers", () => {expect(calculator.compute("8/4/2")).toBe(1);});
+  test.skip("Divides odd number of integers", () => {expect(calculator.compute("30/2/5/3")).toBe(1);});
+  test.skip("Divides large numbers", () => {expect(calculator.compute("144000/1200")).toBe(120);});
+  test.skip("Divides floats", () => {expect(calculator.compute("25.42/3.6")).toBe(7.061111111);});
 });
 
 describe("Wrong input", () =>{
