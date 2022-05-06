e66af0ce2db6a0bf086dca915065ba6b2bfb46f7
Juan Jáuregui <juan@wavesinaroom.com>
2022-05-06 15:53:08 -0500

    [Feature] Add sum large integers test (just in case) -> passed

--- __tests__/calculator-test.spec.js
+++ __tests__/calculator-test.spec.js
@@ -4,6 +4,7 @@ describe("Addition", () =>{
   test("Sums two integers", () =>{expect(calculator.compute("2+3")).toBe(5);});
   test("Sums even number of integers", () =>{expect(calculator.compute("2+3+5+6")).toBe(16);});
   test("Sums odd number of integers", () =>{expect(calculator.compute("2+3+5")).toBe(10);});
+  test("Sums large integers", () =>{expect(calculator.compute("25498+135748+543541")).toBe(704787);});
 });
 
 describe("Substraction", () =>{
